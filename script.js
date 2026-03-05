cfbe74d4e9e83401a8b177705cc6c0f2a43b6f63
Mahamudur10 <mahamudur649@gmail.com>
2026-03-06 01:06:28 +0600

    added all plants

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const categoriesContainer = document.getElementById("categoriesContainer");
+const treesContainer = document.getElementById("treesContainer");
+
 async function loadCategories() {
     // fetch("https://openapi.programming-hero.com/api/categories")
     //     .then((res) => res.json())
@@ -23,4 +25,38 @@ async function loadCategories() {
     });
 
 }
+async function loadTrees() {
+    const res = await fetch("https://openapi.programming-hero.com/api/plants");
+    const data = await res.json();
+    displayTrees(data.plants);
+}
+function displayTrees(trees) {
+    console.log(trees);
+    trees.forEach((tree) => {
+        console.log(tree);
+
+        const card = document.createElement("div");
+        card.className = "card bg-white shadow-sm";
+        card.innerHTML = ` <figure>
+                            <img src="${tree.image}"
+                                alt="${tree.name}" 
+                                title="${tree.name}"
+                                class="h-48 w-full object-cover"    
+                            />
+                        </figure>
+                        <div class="card-body">
+                            <h2 class="card-title">${tree.name}</h2>
+                            <p class="line-clamp-2">A card component has a figure, a body part, and inside body there are title and actions
+                                parts</p>
+                            <div class="badge badge-success">${tree.category}</div>
+                            <div class="flex justify-between">
+                                <h2 class="font-bold text-xl text-[#61e290]">$${tree.price}</h2>
+                                <button class="btn btn-primary bg-[#61e290] border-none">Cart</button>
+                            </div>
+                        </div>`;
+        treesContainer.appendChild(card);
+    });
+}
+
 loadCategories();
+loadTrees();
